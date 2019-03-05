32ac6d07d3c4bc3c35fc23333c1b5bb48a523d06
Rachamim <saarrgett@gett.com>
2019-03-05 13:04:32 +0200

    Removing irrelevant data

--- ConferenceTests/src/main/java/app/common/Constants.java
+++ ConferenceTests/src/main/java/app/common/Constants.java
@@ -1,8 +1,8 @@
 package app.common;
 
 public class Constants {
-    public static final String SECRET = "7rcXVEpk7XgYhYMmjyTQtA8d1GKZB6wglNv0MyMe";
-    public static final String FB_URL = "https://conferencedemo-852e7.firebaseio.com/items/";
+    public static final String SECRET = "secret of Firebase";
+    public static final String FB_URL = "url of your project at Firebase";
     public static final String GOOGLE = "https://www.google.com";
     public static final String SEARCH_FIELD = "//input[@id='lst-ib']";
     public static final String SEARCH_BUTTON = "(//input[@type='submit'])[1]";
--- ConferenceTests/src/main/java/app/executers/TestExecuter.java
+++ ConferenceTests/src/main/java/app/executers/TestExecuter.java
@@ -10,8 +10,11 @@ import java.util.concurrent.TimeUnit;
 
 public class TestExecuter {
 
+    /**
+     * The function executes an infint test
+     */
     public void execute(){
-        System.setProperty("webdriver.chrome.driver", "/Users/Rachamim/Downloads/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "path_to_chrome_driver");
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         boolean stop = true;
@@ -20,7 +23,7 @@ public class TestExecuter {
             try{
                 driver.get(Constants.GOOGLE);
                 WebElement searchField = driver.findElement(By.xpath(Constants.SEARCH_FIELD));
-                searchField.sendKeys("Vilinius places to visit");
+                searchField.sendKeys("String to search at the search field");
                 WebElement searchButton = driver.findElement(By.xpath(Constants.SEARCH_BUTTON));
                 searchButton.submit();
                 Thread.sleep(Constants.FIVE_SECONDS);
